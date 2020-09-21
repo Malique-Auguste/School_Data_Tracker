7639ed54fc6625411f0f58ae98715a80abd35d69
Malique_Auguste <maliqueauguste@gmail.com>
2020-09-20 20:21:04 -0400

    First commit

---
+++ Data.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Data
+{
+    class School
+    {
+        public string name;
+        public Teacher[] teachers;
+        public Student[] students;
+        public Time_Table time_table;
+
+        public School(string name, Teacher[] teachers, Student[] students)
+        {
+            this.name = name;
+            this.teachers = teachers;
+            this.students = students;
+        }
+
+        public int Get_Subject_Popularity(Subject subject_)
+        {
+            int popularity = 0;
+            for (int i = 0; i < students.Length; i++)
+            {
+                for (int j = 0; j < students[i].subjects.Length; j++)
+                {
+                    if (students[i].subjects[j] == subject_)
+                    {
+                        popularity++;
+                    }
+                }
+            }
+
+            return popularity;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} has {1} teachers and {2} students.", name, teachers.Length, students.Length);
+        }
+    }
+
+    class Teacher
+    {
+        public string name;
+        public Subject subject;
+
+        public Teacher(string name, Subject subject)
+        {
+            this.name = name;
+            this.subject = subject;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} teaches {1}.", name, subject);
+        }
+    }
+
+    class Student
+    {
+        public string name;
+        public int form;
+        public Subject[] subjects;
+
+        public Student(string name, int form, Subject[] subjects)
+        {
+            this.name = name;
+            this.form = form;
+            this.subjects = subjects;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0} studies {1} subjects.", name, subjects.Length);
+        }
+    }
+
+    class Time_Table
+    {
+        public int number_of_days; //per week
+        public float day_start;
+        public float period_length; //in minutes
+        public int number_of_periods; //per day
+        public Subject[] subjects;
+        public List<string> time_table_data = new List<string>();
+
+        public Time_Table(int number_of_days, int number_of_periods, float day_start, float period_length, Subject[] subjects)
+        { 
+            if(subjects.Length > number_of_days * number_of_periods)
+            {
+                Console.WriteLine("Error Encountered: too much subjects for time table size.");
+                Console.Read();
+                System.Environment.Exit(1);
+            }
+
+            this.number_of_days = number_of_days;
+            this.number_of_periods = number_of_periods;
+            this.day_start = day_start;
+            this.period_length = period_length;
+            this.subjects = subjects;
+        }
+        
+        public List<int> Generate_Number_Of_Subject_Periods()
+        {
+            subjects.OrderBy(x => x.importance);
+           
+            int total_num_of_periods = number_of_periods * number_of_days;
+            int best_amount_of_periods = subjects.Sum(x => x.importance);
+            List<int> periods = new List<int>();
+            for (int i = 0; i < subjects.Length; i++)
+            {
+                int temp = (int)(subjects[i].importance * ((float)total_num_of_periods / best_amount_of_periods));
+                if(temp == 0)
+                {
+                    temp = 1;
+                }
+                periods.Add(temp);
+            }
+
+            /*
+            int j = 0;
+            while(total_num_of_periods - periods.Sum() > 0 && j != periods.Count)
+            {
+                if(subjects[j].Get_Popularity() > Subject.average_popularity)
+                {
+                    periods[j]++;
+                }
+                j++;
+            }
+            */
+
+            return periods;
+        }
+
+        public List<T> Randomise_List<T>(List<T> list_)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < list_.Count; i++)
+			{
+                int rand_int = rnd.Next(0, list_.Count);
+                T temp = list_[rand_int];
+                list_[rand_int] = list_[i];
+                list_[i] = temp;
+			}
+
+            return list_;
+        }
+
+        public void Generate_Time_Table_Data()
+        {
+            List<int> periods = Generate_Number_Of_Subject_Periods();
+            for (int i = 0; i < subjects.Length; i++)
+			{
+                for (int j = 0; j < periods[i]; j++)
+			    {
+                    Console.Write(subjects[i]);
+                    time_table_data.Add(subjects[i].name + " ");
+			    }
+                Console.WriteLine("");
+			}
+
+            Subject Free_Period = new Subject("Free Period", 0);
+            while(time_table_data.Count < number_of_days * number_of_periods)
+            {
+                time_table_data.Add(Free_Period.name);
+            }
+            
+            time_table_data = Randomise_List(time_table_data);
+            int k = 0;
+            for (int i = 0; i < number_of_days; i++)
+			{
+                Console.WriteLine();
+                for (int j = 0; j < number_of_periods; j++)
+			    {
+                    Console.Write(" | " + time_table_data[k]);
+                    k++;
+			    }
+                Console.Write(" |");
+			}
+        }
+    }
+
+    class Subject
+    {
+        public string name;
+        private int importance_; //on a scale of 1-3, 3 is the most important
+        public int importance
+        {
+            get { return importance_; }
+            set
+            {
+                if (value < 1) { importance_ = 1; }
+                else if (value > 3) { importance_ = 3; }
+                else { importance_ = value; }
+            }
+        }
+
+        private static List<string> subject_types = new List<string>();
+        private static List<int> subject_popularities = new List<int>();
+        public static float average_popularity = 1;
+
+        public Subject(string name, int importance)
+        {
+            this.name = name;
+            this.importance = importance;
+
+            if(!subject_types.Contains(name))
+            {
+                subject_types.Add(name);
+                subject_popularities.Add(0);
+            }
+        }
+
+        public void SetPopularity(int num)
+        {
+            subject_popularities[subject_types.IndexOf(this.name)] += num;
+            average_popularity = (float)subject_popularities.Sum() / subject_popularities.Count;
+        }
+
+        public int Get_Popularity()
+        {
+            return subject_popularities[subject_types.IndexOf(this.name)];
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Data;
+
+namespace SchoolDataTracker
+{
+    class Program
+    {
+        static School school;
+        static Subject Math = new Subject("Mathematics", 3);
+        static Subject Physics = new Subject("Physics", 3);
+        static Subject Chemistry = new Subject("Chemistry", 3);
+        static Subject Business = new Subject("Business", 2);
+        static Subject English = new Subject("English", 2);
+        static Subject Art = new Subject("Art", 1);
+        static Subject Music = new Subject("Music", 1);
+
+        static void Initialise_School()
+        {
+            Teacher[] teachers_ = { new Teacher("Ms. Smith", Math), new Teacher("Mr. Ramoutar", Physics), new Teacher("Mrs. Prince", Business),
+                                    new Teacher("Ms. Ali", English), new Teacher("Mr. Brown", Art), new Teacher("Mr. Parris", Music)};
+            Student[] students_ = { new Student("Joshua", 1, new Subject[] { Math, English, Music }), new Student("Ari", 1, new Subject[] { Math, Physics, Art }),
+                                    new Student("Katelyn", 1, new Subject[] { Chemistry, English, Music }), new Student("Richard", 1, new Subject[] { Physics, Math, Business }),
+                                    new Student("Jabari", 1, new Subject[] { Art, English, Music }), new Student("Zion", 1, new Subject[] { Physics, Chemistry, Music }),
+                                    new Student("Anna", 1, new Subject[] { Physics, English, Music }), new Student("Gregory", 1, new Subject[] { English, Art, Business })};
+            school = new School("1st Achiever's Secondary School", teachers_, students_);
+            Console.WriteLine("School Initialisation Complete");
+        }
+
+        static Time_Table Initialise_Time_Table()
+        {
+            Time_Table time_table = new Time_Table(5, 5, 30, 30, new Subject[] { Math, Physics, Chemistry, Business, English, Art, Music });
+            for (int i = 0; i < time_table.subjects.Length; i++)
+            {
+                time_table.subjects[i].SetPopularity(school.Get_Subject_Popularity(time_table.subjects[i]));
+            }
+            Console.WriteLine("Time Table Initialisation Complete");
+            return time_table;
+        }
+
+        static void Main(string[] args)
+        {
+            Initialise_School();
+            Time_Table time_table = Initialise_Time_Table();
+            time_table.Generate_Time_Table_Data();
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
